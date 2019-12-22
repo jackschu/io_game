0d2e5bcc0131dc8844c917d1a8affc10343ea481
Hamza Mir <hamzasmir@gmail.com>
2019-12-21 19:23:37 -0500

    Lint .js files in cient/ as well

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,24 +3,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
-	entry: './src/client.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-	},
-	module: {
-  		rules: [		  
-		  	{
-				test: /\.(png|svg|jpg|gif)$/,
-				loader: 'file-loader',
-			},
+    entry: './src/client.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: 'file-loader',
+            },
             {
                 test: /\.css$/,
-                use: ['style-loader','css-loader'],
+                use: ['style-loader', 'css-loader'],
             },
-		]
-	},
+        ],
+    },
     devtool: 'eval-source-map',
     devServer: {
         contentBase: './dist',
@@ -30,8 +30,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'io-game',
             template: './src/index.html',
-            inject: 'body'
+            inject: 'body',
         }),
-        new FaviconsWebpackPlugin('./res/favicon.png')
-    ]
+        new FaviconsWebpackPlugin('./res/favicon.png'),
+    ],
 };
