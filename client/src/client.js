982c9812c4741b0de1faee75bbbb9c39a141f55a
Hamza Mir <hamzasmir@gmail.com>
2019-12-21 19:18:21 -0500

    Add linting and lint on commit

--- client/src/client.js
+++ client/src/client.js
@@ -1,9 +1,9 @@
-import { socket } from './socket_io.js'
+import { socket } from './socket_io.js';
 import * as PIXI from 'pixi.js';
 
-let type = 'WebGL'
+let type = 'WebGL';
 if (!PIXI.utils.isWebGLSupported()) {
-    type = 'canvas'
+    type = 'canvas';
 }
 
 let app = new PIXI.Application({
@@ -11,51 +11,49 @@ let app = new PIXI.Application({
     transparent: false, // default: false
     resolution: 1, // default: 1
     autoReszie: true,
-})
+});
 
-document.body.appendChild(app.view)
+document.body.appendChild(app.view);
 
-window.addEventListener('resize', resize)
+window.addEventListener('resize', resize);
 
-function setup() {
-
-}
+function setup() {}
 function makePlayer(x, y, size, username) {
     var name = new PIXI.Text(username, {
         fontFamily: 'Arial',
         fontSize: 24,
         fill: 'white',
         align: 'right',
-    })
-    name.anchor.set(0.5, 0.5)
-    name.position.set(size / 2, size / 2)
+    });
+    name.anchor.set(0.5, 0.5);
+    name.position.set(size / 2, size / 2);
     const rect = new PIXI.Graphics()
         .beginFill(0xff0000)
-        .drawRect(0, 0, size, size)
-    rect.position.set(x - size / 2, y - size / 2)
-    rect.addChild(name)
-    return rect
+        .drawRect(0, 0, size, size);
+    rect.position.set(x - size / 2, y - size / 2);
+    rect.addChild(name);
+    return rect;
 }
 
-const you = makePlayer(app.screen.width / 2, app.screen.height / 2, 200, 'you')
-app.stage.addChild(you)
+const you = makePlayer(app.screen.width / 2, app.screen.height / 2, 200, 'you');
+app.stage.addChild(you);
 // Add it to the stage
 
 // Resize function window
 function resize() {
     // Resize the renderer
-    app.renderer.resize(window.innerWidth, window.innerHeight)
+    app.renderer.resize(window.innerWidth, window.innerHeight);
 
     // You can use the 'screen' property as the renderer visible
     // area, this is more useful than view.width/height because
     // it handles resolution
-    you.position.set(app.screen.width / 2 - 100, app.screen.height / 2 - 100)
+    you.position.set(app.screen.width / 2 - 100, app.screen.height / 2 - 100);
 }
-console.log(socket)
-resize()
+console.log(socket);
+resize();
 
 if (module.hot) {
     module.hot.accept('./socket_io.js', function() {
         console.log('socket_io.js hot-reloaded');
-    });
+    })
 }
