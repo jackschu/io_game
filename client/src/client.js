7aac2a439a9ba379342d202b4bc07e4f0a068e56
Hamza Mir <hamzasmir@gmail.com>
2019-12-22 01:42:11 -0500

    Commit bad code

--- client/src/client.js
+++ client/src/client.js
@@ -1,13 +1,41 @@
 import { socket } from './socket_io.js';
 import * as PIXI from 'pixi.js';
 
+class Player {
+    constructor(pixi_obj) {
+        this.pixi_obj = pixi_obj;
+        this.nextX = pixi_obj.x;
+        this.nextY = pixi_obj.y;
+    }
+    update() {
+        this.pixi_obj.x = this.nextX;
+        this.pixi_obj.y = this.nextY;
+    }
+}
+
+const PLAYER_SIZE = 200;
+const players = {};
+
 socket.onmessage = event => {
-    console.log(event);
-    let data = JSON.parse(event.data);
-    you_x = data.Xpos;
-    you_y = data.Ypos;
-    console.log(you_x);
-    console.log(you_y);
+    let rawData = JSON.parse(JSON.parse(event.data).Body);
+    for (const key in rawData) {
+        const data = rawData[key];
+        if (players[data.id] === undefined) {
+            players[data.id] = new Player(
+                makePlayer(
+                    data.Xpos,
+                    data.Ypos,
+                    PLAYER_SIZE,
+                    key.substring(0, 5)
+                )
+            );
+            app.stage.addChild(players[data.id].pixi_obj);
+        } else {
+            players[data.id].nextX = data.Xpos;
+            players[data.id].nextY = data.Ypos;
+        }
+        console.log(players);
+    }
 };
 
 if (!PIXI.utils.isWebGLSupported()) {
@@ -18,7 +46,7 @@ let app = new PIXI.Application({
     antialias: true, // default: false
     transparent: false, // default: false
     resolution: 1, // default: 1
-    autoReszie: true,
+    autoResize: true,
 });
 
 document.body.appendChild(app.view);
@@ -44,23 +72,19 @@ function makePlayer(x, y, size, username) {
 }
 
 let you;
-let you_x;
-let you_y;
 
 function setup() {
-    you = makePlayer(app.screen.width / 2, app.screen.height / 2, 200, 'you');
-    you_x = you.x;
+    /*
+    you = new Player(app.screen.width / 2, app.screen.height / 2, 200, 'you');
     app.stage.addChild(you);
+    */
     app.ticker.add(delta => gameLoop(delta));
 }
 
 // delta is the fractional lag between frame (0) if not lagging
 function gameLoop(delta) {
-    if (you_x) {
-        you.x = you_x;
-    }
-    if (you_y) {
-        you.y = you_y;
+    for (const key of Object.keys(players)) {
+        players[key].update();
     }
 }
 
