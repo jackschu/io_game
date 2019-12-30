5da83fc1746fd19be2a1e28af21783ebdd369027
Hamza Mir <hamzasmir@gmail.com>
2019-12-29 22:51:00 -0500

    Add spin

--- server/pkg/game/game.go
+++ server/pkg/game/game.go
@@ -16,6 +16,8 @@ type BallInfo struct {
 	Xvel float64
 	Yvel float64
 	Zvel float64
+	Xang float64
+	Yang float64
 }
 
 // TODO replace cwith constant vars
@@ -24,15 +26,18 @@ func NewBallInfo() *BallInfo {
 		Xpos: 750,
 		Ypos: 500,
 		Zpos: 0,
-		Xvel: 0.7,
-		Yvel: 0.7,
-		Zvel: 0.7,
+		Xvel: 0.3,
+		Yvel: 0.3,
+		Zvel: 0.3,
+		Xang: 0,
+		Yang: 0,
 	}
 }
 
 type GameLoop struct {
 	Room    *websocket.Room
 	InfoMap map[string]*PlayerInfo
+	HistMap map[string]*PlayerInfo
 	Ball    *BallInfo
 }
 
@@ -40,6 +45,7 @@ func NewGameLoop(room *websocket.Room) *GameLoop {
 	return &GameLoop{
 		Room:    room,
 		InfoMap: make(map[string]*PlayerInfo),
+		HistMap: make(map[string]*PlayerInfo),
 		Ball:    NewBallInfo(),
 	}
 }
@@ -55,6 +61,13 @@ func playerBallCollide(player *PlayerInfo, ball *BallInfo) bool {
 	deltaX := ball.Xpos - math.Max(rectX, math.Min(ball.Xpos, rectX+player_size))
 	deltaY := ball.Ypos - math.Max(rectY, math.Min(ball.Ypos, rectY+player_size))
 	return (deltaX*deltaX + deltaY*deltaY) < (circle_radius * circle_radius)
+}
+
+func applySpin(ball *BallInfo) {
+	multiple := .0004
+	ball.Yvel += (ball.Yang * ball.Zvel) * multiple
+	ball.Xvel += -(ball.Xang * ball.Zvel) * multiple
+	//ball.Zvel += 0.3 * (ball.Yang*ball.Xvel - ball.Xang*ball.Yvel) * multiple
 }
 
 func (g *GameLoop) Start() {
@@ -75,16 +88,23 @@ func (g *GameLoop) Start() {
 				g.Ball.Zvel *= -1
 			} else if g.Ball.Zpos < 0 && -2*ball_radius < g.Ball.Zpos && g.Ball.Zvel < 0 {
 				bounce := false
-				for _, player := range g.InfoMap {
-					if(playerBallCollide(player, g.Ball)){
+				for key, player := range g.InfoMap {
+					if playerBallCollide(player, g.Ball) {
+						g.Ball.Xang = player.Xpos - g.HistMap[key].Xpos
+						g.Ball.Yang = player.Ypos - g.HistMap[key].Ypos
 						bounce = true
-						break;
+						break
 					}
 				}
 				if bounce {
 					g.Ball.Zvel *= -1
 				} else {
 					g.Ball.Zpos = 800
+					g.Ball.Xang = 0
+					g.Ball.Yang = 0
+					g.Ball.Xvel = 0.3
+					g.Ball.Yvel = 0.3
+					g.Ball.Zvel = 0.3
 				}
 			}
 
@@ -100,6 +120,7 @@ func (g *GameLoop) Start() {
 				g.Ball.Yvel *= -1
 			}
 
+			applySpin(g.Ball)
 			g.Ball.Xpos += float64(dt) * g.Ball.Xvel
 			g.Ball.Ypos += float64(dt) * g.Ball.Yvel
 			g.Ball.Zpos += float64(dt) * g.Ball.Zvel
@@ -143,10 +164,14 @@ func (g *GameLoop) registerMove(action *communication.Action) {
 	}
 	if move == "leave" {
 		delete(g.InfoMap, action.ID)
+		delete(g.HistMap, action.ID)
 		return
 	}
 
 	if json.Valid([]byte(move)) {
+		if curPlayer, ok := g.InfoMap[action.ID]; ok {
+			g.HistMap[action.ID] = &PlayerInfo{curPlayer.Xpos, curPlayer.Ypos}
+		}
 		json.Unmarshal([]byte(move), g.InfoMap[action.ID])
 
 	} else {
