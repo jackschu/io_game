fc07c93170457a340f1b67d6ea41216389d9cb24
Hamza Mir <hamzasmir@gmail.com>
2020-01-07 23:17:04 -0500

    Limit speed of bots

--- server/pkg/game/bot.go
+++ server/pkg/game/bot.go
@@ -1,14 +1,20 @@
 package game
 
+import (
+	"math"
+)
+
 type Bot struct {
-	ID   uint32
-	Game *GameLoop
+	ID       uint32
+	Game     *GameLoop
+	MaxSpeed float32
 }
 
 func NewBot(id uint32, game *GameLoop) *Bot {
 	newBot := &Bot{
-		ID:   id,
-		Game: game,
+		ID:       id,
+		Game:     game,
+		MaxSpeed: 14,
 	}
 	game.AddBot(newBot)
 	return newBot
@@ -20,6 +26,19 @@ func (bot *Bot) Act() {
 
 	botPlayer.Xlast = botPlayer.Xpos
 	botPlayer.Ylast = botPlayer.Ypos
-	botPlayer.Xpos = ball.Xpos
-	botPlayer.Ypos = ball.Ypos
+
+	xDiff := float64(ball.Xpos - botPlayer.Xpos)
+	yDiff := float64(ball.Ypos - botPlayer.Ypos)
+	hypot := math.Hypot(xDiff, yDiff)
+
+	if float32(hypot) <= bot.MaxSpeed {
+		botPlayer.Xpos = ball.Xpos
+		botPlayer.Ypos = ball.Ypos
+	} else {
+		xUnit := float32(xDiff / hypot)
+		yUnit := float32(yDiff / hypot)
+
+		botPlayer.Xpos += xUnit * bot.MaxSpeed
+		botPlayer.Ypos += yUnit * bot.MaxSpeed
+	}
 }
