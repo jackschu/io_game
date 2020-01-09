ed323a59e9c0eda84455dc1fbe165ccd39453943
jackschu <js9pa@virginia.edu>
2020-01-08 23:36:59 -0700

    skeleton and instructions for profiling

--- server/main.go
+++ server/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"flag"
 	"github.com/jackschu/io_game/pkg/websocket"
 	"log"
 	"net/http"
+	"os"
+	"runtime"
+	"runtime/pprof"
+	"time"
 )
+// to profile, go build main.go then ./main -cpuprofile=<anyfilename>
+// wait 50 seconds (adjustable) then kill program
+// go run tool pprof <anyfilename>
+// inside pprof control-d to quit, `web` to view graph, `top10`, `tree` are also useful
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func serveWs(queue *websocket.Queue, w http.ResponseWriter, r *http.Request) {
 	conn, err := websocket.Upgrade(w, r)
@@ -34,8 +44,29 @@ func setupRoutes() {
 }
 
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+	}
+	go stopProfile()
 	// Enable line numbers in logging
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	setupRoutes()
 	log.Fatal(http.ListenAndServe(":8000", nil))
+	return
+
+}
+func stopProfile() {
+	ticker := time.NewTicker(1 * time.Second)
+	for i := 0; i < 50; i++ {
+		select {
+		case <-ticker.C:
+			log.Println(runtime.NumGoroutine())
+		}
+	}
+	pprof.StopCPUProfile()
 }
