953736c6c2ae5d504de86e0a6bcceffa4cf0e121
jackschu <js9pa@virginia.edu>
2020-01-18 15:20:40 -0500

    removed google pb, down to 40Kb main.js

--- client/src/ball.js
+++ client/src/ball.js
@@ -1,7 +1,11 @@
 import { generateCurrentState } from './states';
 import { pointProject } from './utils';
 import Constants from '../../Constants';
-import updates from './updates_pb';
+
+let jsonDescriptor = require('./updates.json');
+let root = protobuf.Root.fromJSON(jsonDescriptor);
+let pbWall = root.lookup('Wall');
+
 export class Ball {
     constructor(zPos) {
         this.pixiObj = new PIXI.Graphics();
@@ -15,7 +19,7 @@ export class Ball {
         if (curState === null) {
             return;
         }
-        if (povWall === updates.Wall.BACK) {
+        if (povWall === pbWall.values.BACK) {
             curState.Zpos = Constants.DEPTH - curState.Zpos;
         }
         let [x0, _] = pointProject(
@@ -25,14 +29,11 @@ export class Ball {
         );
         let radius = x0 - Constants.WIDTH / 2;
 
-        /* this.pixiObj.clear();
-         * this.pixiObj.beginFill(0xff0000); */
         let [x, y] = pointProject(curState.Xpos, curState.Ypos, curState.Zpos);
         let ratio = radius / Constants.BALL_RADIUS;
         this.pixiObj.scale.x = this.pixiObj.scale.y = ratio;
         this.pixiObj.x = x - radius;
         this.pixiObj.y = y - radius;
-        //this.pixiObj.drawCircle(x, y, radius);
         this.zPos = curState.Zpos;
     }
 }
